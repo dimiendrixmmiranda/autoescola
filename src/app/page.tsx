cd4ed1aacc773c894e19fc3f00ad99710ce7af6c
unknown <dimiendrixmmiranda@gmail.com>
2025-01-02 21:20:01 -0300

    update: atualização de cores, teste de melhorias no layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 	const [answers, setAnswers] = useState<(string | null)[]>(Array(listaDePerguntas.length).fill(null));
 	const [resultado, setResultado] = useState<Pergunta[] | null>(null)
-
+	const [acertos, setAcertos] = useState<number | null>(null)
 	const [isClient, setIsClient] = useState(false)
 	useEffect(() => {
 		setIsClient(true)
@@ -58,6 +58,8 @@ export default function Home() {
 			}
 		})
 		setResultado(resultado)
+		const acertos = resultado.filter(pergunta => pergunta.acertou).length;
+		setAcertos(acertos)
 	}
 
 	const handleReiniciarQuestionario = () => {
@@ -68,11 +70,12 @@ export default function Home() {
 		setCurrentQuestionIndex(0)
 		setVisibleTelaRespostas(false)
 		setVisibleTelaInicial(true)
+		setAcertos(0)
 	};
 
 	return (
 		isClient && (
-			<div className="h-screen w-full justify-center items-center bg-[--laranja]">
+			<div className="h-full w-full justify-center items-center bg-[--laranja-claro]">
 				<div className={`justify-center items-center h-screen flex-col gap-6 ${visibleTelaInicial ? 'flex' : 'hidden'}`}>
 					<h2 className="font-bold text-2xl max-w-[300px] text-center uppercase leading-7" style={{ textShadow: '1px 1px 2px black' }}>Simulado de Questões para a Prova Teórica do Detran.</h2>
 					<button
@@ -86,17 +89,16 @@ export default function Home() {
 					</button>
 				</div>
 
-				<div className={`w-full h-screen bg-[--laranja] px-2 py-4 flex-col ${visibleTelaJogo ? 'flex' : 'hidden'}`}>
-					<div className="flex flex-col gap-2">
+				<div className={`w-full h-screen px-2 py-4 flex-col ${visibleTelaJogo ? 'flex' : 'hidden'} lg:flex-row lg:gap-4 xl:max-w-[1300px] xl:mx-auto xl:gap-8`}>
+					<div className="flex flex-col gap-2 max-w-[700px] w-full">
 						<h2 className="text-xl leading-6 font-bold">{listaDePerguntas[currentQuestionIndex].pergunta}</h2>
 						<ul className="flex flex-col gap-1">
 							{listaDePerguntas[currentQuestionIndex].alternativas.map((alternativa, index) => (
 								<li
 									key={index}
-									className={`flex gap-1 px-2 py-1 cursor-pointer border-4 rounded-lg text-lg ${answers[currentQuestionIndex] === alternativa.conteudo ? 'bg-green-500 border-green-700' : 'bg-[--laranja-claro] border-[--laranja-escuro]'
+									className={`flex gap-1 px-2 py-1 cursor-pointer border-2 rounded-lg text-lg ${answers[currentQuestionIndex] === alternativa.conteudo ? 'bg-green-600 border-green-800 text-white' : 'bg-[--bege] text-black'
 										}`}
 									onClick={() => handleOptionClick(alternativa.conteudo)}
-									style={{ textShadow: '1px 1px 2px black' }}
 								>
 									<span>{String.fromCharCode(65 + index)}-)</span>
 									<p>{alternativa.conteudo}</p>
@@ -124,12 +126,12 @@ export default function Home() {
 					</div>
 
 					{/* Navegação por botões numerados */}
-					<div className="max-w-[300px] mt-4">
+					<div className="max-w-[300px] h-fit mt-4 bg-[--laranja-escuro] p-2 mx-auto lg:self-start lg:mt-10 xl:mx-auto" style={{boxShadow: '1px 1px 2px 2px black'}}>
 						<ul className="grid grid-cols-5 gap-2 justify-center">
 							{listaDePerguntas.map((_, i) => (
 								<li key={i} className="flex justify-center items-center">
 									<button
-										className={`w-[50px] h-[50px] mx-auto border-[3px] border-orange-600 rounded-lg text-xl font-black ${currentQuestionIndex === i ? 'bg-[--vermelho]' : 'bg-[--laranja-claro] text-white'}`}
+										className={`w-[50px] h-[50px] mx-auto border-[3px] rounded-lg text-xl font-black ${currentQuestionIndex === i ? 'bg-[--vermelho] border-black' : 'bg-[--laranja-claro] border-[--laranja]'}`}
 										onClick={() => handleQuestionNavigation(i)}
 										style={{ textShadow: '1px 1px 2px black' }}
 									>
@@ -138,24 +140,26 @@ export default function Home() {
 								</li>
 							))}
 						</ul>
-						<div className="w-full text-2xl font-black bg-red-600 py-2 mt-4">
+						<div className="w-full text-2xl font-black bg-[--vermelho] py-2 mt-4">
 							<button className="w-full h-full uppercase" onClick={() => handleTerminarQuestionario()}>Terminar</button>
 						</div>
 					</div>
 				</div>
 
-				<div className={`${visibleTelaRespostas ? 'flex' : 'hidden'} flex-col bg-orange-500 p-3`}>
-					<ul className="flex flex-col gap-6">
+				<div className={`${visibleTelaRespostas ? 'flex' : 'hidden'} flex-col p-3 max-w-[1200px] mx-auto`}>
+					<h2 className="text-3xl font-black text-center mt-3 mb-6">Você acertou {acertos}/{resultado != undefined && resultado?.length - 1}</h2>
+
+					<ul className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
 						{
 							resultado != null && resultado.map((pergunta, i) => {
 								return (
-									<li key={i} className={`flex flex-col gap-1 p-2 border-2 border-black ${pergunta.acertou ? 'bg-green-600' : 'bg-red-600'}`}>
+									<li key={i} className={`flex flex-col gap-2 p-2 border-2 border-black ${pergunta.acertou ? 'bg-green-600' : 'bg-red-600'} xl:gap-4`}>
 										<h2 className="text-lg leading-5 font-bold">{pergunta.pergunta}</h2>
 										<ul className="flex flex-col gap-1">
 											{
 												pergunta.alternativas.map((alternativa, j) => {
 													return (
-														<li key={j} className={`flex relative px-1 gap-1 font-medium ${alternativa.certa ? 'bg-green-600' : ''}`}>
+														<li key={j} className={`flex relative px-2 py-1 gap-1 font-medium border-2 ${alternativa.certa ? 'bg-green-600 border-green-800' : 'border-black'}`}>
 															<span>{String.fromCharCode(65 + j)}-)</span>
 															<p>{alternativa.conteudo}</p>
 															{
@@ -168,14 +172,14 @@ export default function Home() {
 												})
 											}
 										</ul>
-										<p className={`font-black ${pergunta.acertou ? 'bg-green-400' : 'bg-red-400'} p-1 mt-1`} style={{ textShadow: '1px 1px 1px black' }}>{pergunta.acertou ? 'Acertou!' : 'Errou!'}</p>
+										<p className={`font-black ${pergunta.acertou ? 'bg-green-400' : 'bg-red-400'} p-1`} style={{ textShadow: '1px 1px 1px black' }}>{pergunta.acertou ? 'Acertou!' : 'Errou!'}</p>
 									</li>
 								)
 							})
 						}
 					</ul>
 
-					<button className="bg-[--azul] p-2 text-2xl uppercase font-bold border-[3px] border-black mt-6" onClick={() => handleReiniciarQuestionario()}>Recomeçar</button>
+					<button className="bg-[--vermelho] p-2 text-2xl uppercase font-bold border-[3px] border-black mt-6" onClick={() => handleReiniciarQuestionario()}>Recomeçar</button>
 				</div>
 			</div>
 		)
